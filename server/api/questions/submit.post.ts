a5960182d7ee5f9448c8f855c497d98568ff558f
leen-neel <and24903@gmail.com>
2024-12-09 19:44:14 +0530

    feature: added exam submission

--- server/api/questions/submit.post.ts
+++ server/api/questions/submit.post.ts
@@ -2,5 +2,22 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const token = getHeader(event, "Authorization");
 
-  console.log(body);
+  console.log({ response: body });
+
+  try {
+    const submitExam = await $fetch(
+      "http://localhost:8998/api/v1/exam/submit-exam/6755cc46cf6a991bf8204af9",
+      {
+        method: "POST",
+        body: {
+          response: body,
+        },
+        headers: {
+          Authorization: token!,
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 });
